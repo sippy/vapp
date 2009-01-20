6d4a586c494ec4a394fc37c72ac2587ee09a2abd
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2009-01-20 07:49:05 +0000

    Fix plurals list for Russian.

--- trunk/scripts/vapp_prompt_utils.py
+++ trunk/scripts/vapp_prompt_utils.py
@@ -254,7 +254,7 @@ class PhraseContainer:
 	if (lang == 'en' or lang == 'es' or lang == 'hy' or lang == 'fr'):
 	    self.__plurals = [ 0, 1 ]
 	elif (lang == 'ru'):
-	    self.__plurals = [ 1, 2, 4 ]
+	    self.__plurals = [ 1, 2, 5 ]
 	elif (lang == 'ar'):
 	    self.__plurals = [ 1, 2, 3 ]
         elif (lang == 'th' or lang == 'zh' or lang == 'ja' or lang == 'tr'):

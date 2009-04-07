c032ea190263dd45998f94efd51b6ccc8fbdb1c2
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2009-04-07 15:36:56 +0000

    Install compiled gettext files.

--- po/compilepo.sh
+++ po/compilepo.sh
@@ -46,6 +46,7 @@ fi
 
 if [ -f "$POFILE" ]
 then
+    echo msgfmt -o "${OUTPUT_DIR}/${DOMAIN}.mo" -c "${POFILE}"
     msgfmt -o "${OUTPUT_DIR}/${DOMAIN}.mo" -c "${POFILE}"
 else
     echo "Cannot find the ${POFILE} file!"

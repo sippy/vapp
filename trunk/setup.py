a82cde2fa61fa482cb480da3a37d317e0b25297e
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2009-06-11 13:12:39 +0000

    Add Arabic prompts.

--- trunk/setup.py
+++ trunk/setup.py
@@ -28,7 +28,7 @@ from distutils.command.install import install as _install
 from distutils.core import setup, Extension
 import glob
 
-VERSION = "0.02"
+VERSION = "0.03"
 
 LICENSE = """
         The vapp library is distributed under version 2 of GNU Public 

66ef753e7c915ec38ac45956273c84d441b68d70
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2010-04-28 11:24:27 +0000

    Version 0.15.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.command.install import install as _install
 from distutils.core import setup, Extension
 import glob
 
-VERSION = "0.14"
+VERSION = "0.15"
 
 LICENSE = """
         The vapp library is distributed under version 2 of GNU Public 

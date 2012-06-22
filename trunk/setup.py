ec36f493e07ec0ba730ad807d8506767bd31f154
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2012-06-22 06:20:58 +0000

    Version 0.24

--- trunk/setup.py
+++ trunk/setup.py
@@ -28,7 +28,7 @@ from distutils.command.install import install as _install
 from distutils.core import setup, Extension
 import glob
 
-VERSION = "0.23"
+VERSION = "0.24"
 
 LICENSE = """
         The vapp library is distributed under version 2 of GNU Public 

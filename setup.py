27fe92fc9e78d88902a909b554c96fdcf4421fff
pai1ripe <pai1ripe@68f5fb6f-58f1-4bfb-9267-f1f7e906c9ca>
2014-12-11 13:04:50 +0000

    Bugfix: Prevent memory leak when session handler throws an exception. At least the AsteriskVMRecord plugin was affected.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.command.install import install as _install
 from distutils.core import setup, Extension
 import glob
 
-VERSION = "0.27"
+VERSION = "0.28"
 
 LICENSE = """
         The vapp library is distributed under version 2 of GNU Public 

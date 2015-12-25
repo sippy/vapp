7cb8367d75484db5536ee4e4e380a050e4a9e539
Andriy Pylypenko <bamby@sippysoft.com>
2015-12-25 16:33:42 +0200

    Version 0.33

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.command.install import install as _install
 from distutils.core import setup, Extension
 import glob
 
-VERSION = "0.32"
+VERSION = "0.33"
 
 LICENSE = """
         The vapp library is distributed under version 2 of GNU Public 
